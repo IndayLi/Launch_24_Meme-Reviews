c523973327b027c1619ef435fc3c362509cc998e
DC Anthony <dolan.c.anthony@gmail.com>
2019-04-23 15:09:31 -0400

    Took out comment on application_controller.rb.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # below is step 9
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
