01900fb59faf2e66913a0da2f50b817056e8e357
DC Anthony <dolan.c.anthony@gmail.com>
2019-04-23 14:02:16 -0400

    Migrated add_role_to_users file. Edited memes_controller.rb to include the before_action, authorize_admin, and authorize_user methods. Edited user.rb file to add the admin? method.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_144216) do
+ActiveRecord::Schema.define(version: 2019_04_23_173538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_04_23_144216) do
     t.string "first_name"
     t.string "last_name"
     t.string "username"
+    t.string "role", default: "member", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
