f26528b8c6f0bc809c487cc1bad006c7dcfd49d0
DC Anthony <dolan.c.anthony@gmail.com>
2019-04-25 17:18:51 -0400

    Fixed enzyme tests for the ShowContainer and added fetchMock to get all the tests to pass.

--- spec/javascript/showMeme.js
+++ spec/javascript/showMeme.js
@@ -1,29 +1,31 @@
-import testHelper from "./testHelper";
-
+import fetchMock from "fetch-mock";
 import ShowContainer from "../../app/javascript/react/components/ShowContainer";
 
 describe("ShowContainer", () => {
-  let title,
-    imageUrl,
-    description,
-    wrapper;
+  let wrapper, meme;
 
   beforeEach(() => {
     jasmineEnzyme();
-    wrapper = mount(
-      <ShowContainer
-        title="first meme"
-        imageUrl="www.examplememe.com"
-        description="this is the first meme"
-      />
-    );
+    meme = {
+      id: 1,
+      title: "Momo",
+      imageUrl: "https://i.imgur.com/4wVB82o.png",
+      description: "Let's play a game...!",
+      user_id: 1
+    };
+    fetchMock.get(`/api/v1/memes/1`, {
+      status: 200,
+      body: { meme }
+    });
+    wrapper = mount(<ShowContainer params={{ id: 1 }}/>);
   });
 
-  it("should render the ShowContainer component with its specific props", () => {
-    expect(wrapper.find(ShowContainer).props()).toEqual({
-      title: "first meme",
-      imageUrl: "www.examplememe.com",
-      description: "this is the first meme"
-    });
+  afterEach(fetchMock.restore);
+
+  it("renders an h2 with meme title", (done) => {
+    setTimeout(() => {
+      expect(wrapper.find(ShowContainer)).toBePresent();
+      done();
+    }, 0);
   });
 });
