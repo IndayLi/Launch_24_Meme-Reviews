62714171d6a82ebc2d85c1f24bcf989eae7b613d
Anthonyhelka <helkaanthony1997@gmail.com>
2019-04-29 14:37:23 -0400

    Refactored to remove extra whitespace

--- app/javascript/react/components/ReviewsFormContainer.js
+++ app/javascript/react/components/ReviewsFormContainer.js
@@ -27,28 +27,28 @@ class ReviewsFormContainer extends Component {
 
     fetch(`/api/v1/memes/${this.props.meme.id}/reviews`, {
       credentials: "same-origin",
-    	method: "POST",
-    	body: JSON.stringify(reviewPayload),
+      method: "POST",
+      body: JSON.stringify(reviewPayload),
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       }
-  	})
-  	.then(response => {
-  		if (response.ok) {
-  			return response;
-  		} else {
-  			let errorMessage = `${response.status} (${response.statusText})`,
-  			error = new Error(errorMessage);
-  			throw(error);
-  		};
-  	})
-  	.then(response => response.json())
-  	.then(body => {
-      // this.setState({reviews: this.state.reviews.concat(body.review) });
     })
-  	.catch(error => console.error(`Error in fetch: ${error.message}`));
-
+    .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+        error = new Error(errorMessage);
+        throw(error);
+      };
+    })
+    .then(response => response.json())
+    .then(body => {
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`)
+    );
+    
     this.clearForm();
   };
 
