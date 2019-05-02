530bf3ed036764e91f10a1caec9fd1b0a8befd31
Anthonyhelka <helkaanthony1997@gmail.com>
2019-05-02 11:01:01 -0400

    Added links for each meme, as well as the functionality and testing for deleting reviews.

--- app/javascript/react/components/MemeTile.js
+++ app/javascript/react/components/MemeTile.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
 
 const MemeTile = props => {
   return(
     <div className="tile">
       <dd>
+      <Link to={`/memes/${props.id}`}>
         <dl><img src={props.imageUrl}/></dl>
+      </Link>
       </dd>
     </div>
   )
