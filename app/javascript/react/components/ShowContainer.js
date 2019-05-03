7c67fe72c8074ea0de498497337e934add9f1e7a
nkim1225 <nkim1225@gmail.com>
2019-05-03 09:38:27 -0400

    meme buttons appear depending on user

--- app/javascript/react/components/ShowContainer.js
+++ app/javascript/react/components/ShowContainer.js
@@ -103,7 +103,9 @@ class ShowContainer extends Component {
 
   render() {
     let memeId = this.props.params.id;
-    let form;
+    let form = "";
+    let memeOptions = "";
+
     if (this.state.showEdit) {
       form = (
         <div>
@@ -115,8 +117,39 @@ class ShowContainer extends Component {
           />
         </div>
       );
-    } else {
-      form = "";
+    }
+    if (
+      this.state.currentUser.id === this.state.meme.user_id ||
+      this.state.currentUser.role === "admin"
+    ) {
+      memeOptions = (
+        <div className="memeButtons">
+          <div>
+            <div onClick={this.toggleEdit}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              >
+                <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
+                <path d="M0 0h24v24H0z" fill="none" />
+              </svg>
+            </div>
+          </div>
+          <div onClick={this.deleteMeme}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+            >
+              <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
+              <path d="M0 0h24v24H0z" fill="none" />
+            </svg>
+          </div>
+        </div>
+      );
     }
     return (
       <div id="show-page">
@@ -126,32 +159,7 @@ class ShowContainer extends Component {
         <div id="show-text">
           <h2>
             {this.state.meme.title}
-            <div className="memeButtons">
-              <div>
-                <div onClick={this.toggleEdit}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
-                    <path d="M0 0h24v24H0z" fill="none" />
-                  </svg>
-                </div>
-              </div>
-              <div onClick={this.deleteMeme}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
-                  <path d="M0 0h24v24H0z" fill="none" />
-                </svg>
-              </div>
-            </div>
+            {memeOptions}
           </h2>
           <div id="meme-description">
             <p>
